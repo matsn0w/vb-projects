003f3f053850a8d34d76ef928532e17ef5b90433
Bart Scholtus <138978@intern.ichthuscollege.nl>
2017-10-26 13:57:00 +0200

    Added Autoverhuur

---
+++ Autoverhuur/Autoverhuur/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Autoverhuur")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Ichthus College")>
+<Assembly: AssemblyProduct("Autoverhuur")>
+<Assembly: AssemblyCopyright("Copyright © Ichthus College 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e83a9766-0edc-46b2-a8ab-e21fc1677bb0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
